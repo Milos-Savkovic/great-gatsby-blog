0296b10b1b53f886bed9a47cfe93e867dfd9650a
Milos Savkovic <milos.savkovic@jsguru.io>
2018-10-01 15:33:17 +0200

    My gatsby blog site

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Link} from "gatsby"
+import Footer from "./footer"
+
+export default () => (
+  <div>
+    <h1>Asd</h1>
+    <Link to="/">Home</Link>
+    <Footer tekst="sahbazzzzzzz"/>
+  </div>
+)
